26b1e8e6e76c2ded9d04e43e9426de08172c6561
Suman Shadangi <129254642+sumshadz@users.noreply.github.com>
2023-06-09 11:27:06 +0530

    Optimized approach Repeat_and_Missing_Number.cpp

--- Repeat_and_Missing_Number.cpp
+++ Repeat_and_Missing_Number.cpp
@@ -14,3 +14,29 @@ pair<int,int> missingAndRepeating(vector<int> &arr, int n)
 	}
 	return ans;
 }
+
+
+// optimized approach with T.C = O(n) and S.C = O(1)
+
+#include <bits/stdc++.h>
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n1)
+{
+	// x-y=s-sn
+	//(x-y)(x+y)=s2-s2n ==> x+y=(s2-s2n)/(x-y)
+	// we are using long long becase overflow may happen
+	long long n=(long long)n1;
+	long long sn=(n*(n+1))/2;
+	long long sn2=(n*(n+1)*(2*n+1))/6;
+	long long s=0,s2=0;
+	for(int i=0;i<n;i++){
+		s+=(long long)arr[i];
+		s2+=(long long)arr[i]*(long long)arr[i];
+	}
+	long long diff=s-sn; //x-y
+	long long sum=(s2-sn2)/diff; 
+	long long x=(sum+diff)/2;
+	long long y= x-diff;
+	return {(int)y,(int)x};
+	
+}
